f9819760ac461fe47251f8b2b1e54c2389ee6f61
breitburg <contact@breitburg.me>
2019-10-21 03:37:43 +0300

    Added fonts and absolute path calculating

--- rocket/render/__init__.py
+++ rocket/render/__init__.py
@@ -1,4 +1,4 @@
-from os import environ
+from os import environ, path
 import pygame
 
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
@@ -6,7 +6,9 @@ pygame.init()
 
 pygame.display.set_caption('Rocket')
 surface = pygame.display.set_mode((800, 600))
-font = pygame.font.SysFont('product_sans', 28)
+font = pygame.font.Font(path.join(
+    __file__.replace('/render/__init__.py', ''),
+    'font', 'product_sans_regular.ttf'), 26)
 clock = pygame.time.Clock()
 touches = []
 
--- test.py
+++ test.py
@@ -1,30 +1,10 @@
-from rocket import launch, material, widget
+from rocket import launch, material
 
 
-class MyApp (widget.Widget):
-    hello = 0
-
-    def build(self):
-        return material.Scaffold(
-            app_bar=material.AppBar(
-                title=widget.Text('Material App Demo')
-            ),
-            body=material.Page(
-                widget.Padding(
-                    widget=widget.Column(
-                        children=(
-                            widget.Text(f'Number: {self.hello}', color=(20, 20, 20)),
-                            material.PushButton('Press here', pressed=self.on_button_press)
-                        ),
-                        spacing=20
-                    ),
-                    padding=20
-                )
-            )
+launch(
+    material.Page(
+        widget=material.PushButton(
+            text='Hello'
         )
-
-    def on_button_press(self):
-        self.hello += 1
-
-
-launch(MyApp())
+    )
+)
