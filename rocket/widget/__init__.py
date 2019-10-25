321d66de874353def0c05b4d557af77303f360fd
breitburg <contact@breitburg.me>
2019-10-25 17:09:49 +0300

    Moved spacer to layouts

--- rocket/widget/__init__.py
+++ rocket/widget/__init__.py
@@ -3,8 +3,8 @@ from rocket.widget.core import Widget
 from rocket.widget.core.rectangle import Rectangle
 from rocket.widget.core.text import Text
 from rocket.widget.core.ellipse import Ellipse
-from rocket.widget.core.spacer import Spacer
 
 from rocket.widget.layout.padding import Padding
 from rocket.widget.layout.stack import Column, Row
 from rocket.widget.layout.overlay import Overlay
+from rocket.widget.layout.spacer import Spacer
--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-from rocket import launch, material, widget
+from rocket.widget import *
+from rocket.material import *
+from rocket import launch, auto_reload
+
+@auto_reload
+class MyApp (Widget):
+    material_app = App(None)
+
+    def build(self):
+        self.material_app.home = Scaffold(
+            app_bar=AppBar(
+                child=Text('Hi')
+            ),
+            body=Padding(
+                child=PushButton(
+                    child=Text('Press here'),
+                    pressed=self.on_display_alert
+                ),
+                padding=20
+            )
+        )
+        return self.material_app
+
+    def on_display_alert(self, button):
+        self.material_app.display_alert('a', 'aaa', 'a')
 
 
-launch(
-    material.Alert(
-        title=widget.Text('Test Alert', color=(0, 0, 0), font='bold'),
-        subtitle=widget.Text('ma', color=(0, 0, 0))
-    )
-)
+launch(MyApp())
