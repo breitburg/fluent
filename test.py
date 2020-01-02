24b380ad6cdfe5a656bb741b7653bc49ecd5f321
breitburg <contact@breitburg.me>
2020-01-02 03:08:53 +0300

    Added assets manifest file

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from fluent.essential import *
+from fluent.debug import auto_reload
+from fluentmd import Button
+
+@auto_reload
+class App(Widget):
+    def build(self):
+        return Button(
+            child=Text('Click me', color=Color.white),
+            color=Color.blue
+        )
+
 
 launch(
-    target=Text('Hello, Flutter', color=Color.white)
+    target=App()
 )
